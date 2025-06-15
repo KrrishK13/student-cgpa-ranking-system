0df7ff12f543499197b523c0d2b1a49f994c2978
krrish khushani <krish.khushani@avantika.edu.in>
2025-06-15 21:39:33 +0530

    Day 4: Updated README with Day 4 progress

--- main.py
+++ main.py
@@ -29,19 +29,61 @@ def add_student():
     students.append(student)
     print("✅ Student added successfully!")
 
+def update_student():
+    roll = input("Enter Roll Number of the student to update: ")
+    
+    for student in students:
+        if student['roll'] == roll:
+            print(f"Current Name: {student['name']}")
+            print(f"Current CGPA: {student['cgpa']}")
+            student['name'] = input("Enter New Name: ")
+            
+            try:
+                cgpa = float(input("Enter New CGPA (0 - 10): "))
+                if 0 <= cgpa <= 10:
+                    student['cgpa'] = cgpa
+                    print("✅ Student updated successfully!")
+                else:
+                    print("❌ CGPA must be between 0 and 10.")
+            except ValueError:
+                print("❌ Invalid CGPA input.")
+            return
+
+    print("❌ Student with that Roll Number not found.")
+
+def delete_student():
+    roll = input("Enter Roll Number of the student to delete: ")
+    
+    for student in students:
+        if student['roll'] == roll:
+            students.remove(student)
+            print("✅ Student deleted successfully!")
+            return
+
+    print("❌ Student with that Roll Number not found.")
+
+
+
 def show_menu():
     print("\n📚 Student CGPA Ranking System")
     print("1. Add Student")
-    print("2. Exit")
+    print("2. Update Student")
+    print("3. Delete Student")
+    print("4. Exit")
+
 
 def main():
     while True:
         show_menu()
-        choice = input("Enter your choice (1 or 2): ")
+        choice = input("Enter your choice (1–4): ")
 
         if choice == '1':
             add_student()
         elif choice == '2':
+            update_student()
+        elif choice == '3':
+            delete_student()
+        elif choice == '4':
             print("Goodbye!")
             break
         else:
